4a1366c57de8d5c1d088c2f111528b65475b5ce1
Tub <tubandzits@gmail.com>
2025-09-03 15:52:41 -0700

    fix: cast Review model through unknown to satisfy TypeScript

--- src/lib/models/Review.ts
+++ src/lib/models/Review.ts
@@ -193,7 +193,7 @@ ReviewSchema.methods.report = function() {
   return this.save()
 }
 
-const Review = models.Review as IReviewModel || model<IReview, IReviewModel>('Review', ReviewSchema)
+const Review = (models.Review as unknown as IReviewModel) || model<IReview, IReviewModel>('Review', ReviewSchema)
 
 export default Review
 
